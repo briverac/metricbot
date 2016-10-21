0d9e259443bb64b61d0368ea4718a90f69ea4c1c
Bryan Rivera <brivera@growthaccelerationpartners.com>
2016-10-21 12:20:10 -0600

    Update llama_a code

--- pingpongbot.rb
+++ pingpongbot.rb
@@ -9,13 +9,28 @@ class PingPongBot < SlackRubyBot::Bot
     text = match[:expression]
     client.say(text: "<@#{data.user}> toma #{text}", channel: data.channel, gif: text)
   end
+
   command 'llama_a' do |client, data, match|
-    text = match[:expression]
-    text.slice! "<@"
-    text.slice! ">"
-    user = client.users[text]
+    id = match[:expression]
+    id.slice!("<@")
+    id.slice!(">")
+    user = client.users[id]
+    image = get_image(user.profile)
     real_name = user.real_name
-    client.say(text: "<@#{text}> #{real_name.upcase} te buscan, si no contestas les doy tu correo para que hagan spam #{user.profile.email} y tu cara #{user.profile.image_1024}", channel: data.channel)
+    client.say(text: "<@#{id}> #{real_name.upcase} te buscan, si no contestas les doy tu correo para que hagan spam #{user.profile.email} y tu cara #{image}", channel: data.channel)
+  end
+
+  class << self
+    private
+    def get_image(profile)
+      return profile.image_1024 unless profile.image_1024.nil?
+      return profile.image_512 unless profile.image_512.nil?
+      return profile.image_192 unless profile.image_192.nil?
+      return profile.image_72 unless profile.image_72.nil?
+      return profile.image_48 unless profile.image_48.nil?
+      return profile.image_32 unless profile.image_32.nil?
+      return profile.image_24 unless profile.image_24.nil?
+    end
   end
 end
 
