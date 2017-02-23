afff8046e0efb166c709212351e680fcf7ae2343
Bryan Rivera <brivera@growthaccelerationpartners.com>
2017-02-23 11:26:38 -0600

    Add first version of metric request

---
+++ metric.rb
@@ -0,0 +1,34 @@
+require 'HTTParty'
+
+class Metric
+  attr_reader :url
+  attr_reader :metric_id
+  attr_reader :location
+  attr_reader :compute_period
+
+  def initialize(url, metric_id, compute_period, location)
+    @url = url
+    @metric_id = metric_id
+    @compute_period = compute_period
+    @location = location
+  end
+
+  def response
+    HTTParty.get(request, headers: headers)
+  end
+
+  private
+
+  def request
+    "#{url}/api/2/metrics/#{metric_id}/values.json?location=#{location}&compute_period=#{compute_period}"
+  end
+
+  def headers
+    {
+      'Content-Type' => 'application/json',
+      'Authorization' => 'Basic VVNFUk5BTUUxOnBhc3N3b3Jk',
+      'X-API-KEY' => '9a3d7cc7806c4390e07399d362c6a04a491c5cc5ad27a16aa1f22b678b92888d'
+    }
+  end
+
+end
--- pingpongbot.rb
+++ pingpongbot.rb
@@ -1,6 +1,18 @@
 require 'slack-ruby-bot'
+require_relative 'Metric'
 
 class PingPongBot < SlackRubyBot::Bot
+
+  help do
+    title 'PingPongBot'
+    desc 'Do funny stuff'
+
+    command 'quiero' do
+      desc 'Show you a picture that represent whatever you write'
+      long_desc 'It takes the phrase you enter and look for an image that represent it'
+    end
+  end
+
   command 'ping' do |client, data, match|
     client.say(text: 'pong', channel: data.channel)
   end
@@ -18,6 +30,30 @@ class PingPongBot < SlackRubyBot::Bot
     image = get_image(user.profile)
     real_name = user.real_name
     client.say(text: "<@#{id}> #{real_name.upcase} te buscan, si no contestas les doy tu correo para que hagan spam #{user.profile.email} y tu cara #{image}", channel: data.channel)
+  end
+
+  command 'get_server' do |client, data, match|
+    client.say(text: @server || 'Server not setted', channel: data.channel)
+  end
+
+  command 'set_server' do |client, data, match|
+    text = match[:expression]
+    return client.say(text: 'You must enter the server name', channel: data.channel) unless text
+    text.slice!("<")
+    text.slice!(">")
+    @server = text
+    client.say(text: @server || 'Server setted', channel: data.channel)
+  end
+
+  command 'metric' do |client, data, match|
+    return client.say(text: 'Please first set the server', channel: data.channel) unless @server
+    params = match[:expression].split(' ')
+    if params.size != 3
+      client.say(text: 'You must enter the metric_id, the computed_period and the location', channel: data.channel)
+    else
+      metric = Metric.new(@server, params[0], params[1], params[2])
+      client.say(text: metric.response, channel: data.channel)
+    end
   end
 
   class << self
